79bdfeb7a6564412a88286994c846394f3390197
doan02012004 <doana1k24@gmail.com>
2024-06-10 23:49:03 +0700

    form-add-fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AddComponent } from './form/add/add.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"",component:AppComponent,children:[
+    {path:"add", component:AddComponent}
+  ]},
+
+];
